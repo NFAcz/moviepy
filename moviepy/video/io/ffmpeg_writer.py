49b777f92ffe7623c2d1a94345234d8183b79105
Jonas Svatos <jonas.svatos@nfa.cz>
2019-08-19 15:13:16 +0200

    print FFmpeg commands

--- moviepy/video/io/ffmpeg_writer.py
+++ moviepy/video/io/ffmpeg_writer.py
@@ -123,6 +123,7 @@ class FFMPEG_VideoWriter:
         if os.name == "nt":
             popen_params["creationflags"] = 0x08000000  # CREATE_NO_WINDOW
 
+        print cmd
         self.proc = sp.Popen(cmd, **popen_params)
 
 
